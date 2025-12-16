0243f0c8396e4cc202b7c1a950d79d45b31240d5
SaberLS <patryk.platek19@gmail.com>
2025-12-16 18:24:53 +0100

    feat(operation): add static class with map of operations assigned to roles

---
+++ src/main/java/pl/wszib/java/advanced/security/RolePermissions.java
@@ -0,0 +1,31 @@
+package pl.wszib.java.advanced.security;
+
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
+
+import pl.wszib.java.advanced.model.Role;
+import pl.wszib.java.advanced.model.operation.Operation;
+
+public class RolePermissions {
+  private static final Map<Role, Set<Operation>> PERMISSIONS = new EnumMap<>(Role.class);
+
+  static {
+    PERMISSIONS.put(Role.USER, EnumSet.of(
+        Operation.LIST_BOOKS,
+        Operation.BORROW_BOOK,
+        Operation.EXIT));
+
+    PERMISSIONS.put(Role.ADMIN, EnumSet.of(
+        Operation.LIST_BOOKS,
+        Operation.BORROW_BOOK,
+        Operation.ADD_BOOK,
+        Operation.REMOVE_BOOK,
+        Operation.EXIT));
+  }
+
+  public static Set<Operation> getPermissions(Role role) {
+    return PERMISSIONS.get(role);
+  }
+}
