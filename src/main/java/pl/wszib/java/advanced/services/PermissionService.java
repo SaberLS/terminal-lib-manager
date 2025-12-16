5a78ec04a24f7ee653b0767e00f2c74a44ff3a23
SaberLS <patryk.platek19@gmail.com>
2025-12-16 18:28:43 +0100

    feat(operation): add permission service provides available based on user permission operations

---
+++ src/main/java/pl/wszib/java/advanced/services/PermissionService.java
@@ -0,0 +1,16 @@
+package pl.wszib.java.advanced.services;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import pl.wszib.java.advanced.model.User;
+import pl.wszib.java.advanced.model.operation.Operation;
+import pl.wszib.java.advanced.security.RolePermissions;
+
+@Service
+public class PermissionService {
+  public Set<Operation> getPermissions(User user) {
+    return RolePermissions.getPermissions(user.getRole());
+  }
+}
