e80e19160cf3ede8af2f0a6463f1b509906cd3c9
SaberLS <patryk.platek19@gmail.com>
2025-12-16 18:33:28 +0100

    feat(operation): add description field with getter to operation

--- src/main/java/pl/wszib/java/advanced/model/operation/Operation.java
+++ src/main/java/pl/wszib/java/advanced/model/operation/Operation.java
@@ -1,9 +1,16 @@
 package pl.wszib.java.advanced.model.operation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Operation {
-  LIST_BOOKS,
-  BORROW_BOOK,
-  ADD_BOOK,
-  REMOVE_BOOK,
-  EXIT,
+  LIST_BOOKS("List all books"),
+  BORROW_BOOK("Borrow a book"),
+  ADD_BOOK("Add a new book"),
+  REMOVE_BOOK("Remove a book"),
+  EXIT("Exit application");
+
+  private final String description;
 }
