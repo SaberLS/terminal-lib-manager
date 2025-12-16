22ba1e2122fecf3bbb87d1b1ea761e048007c7c4
SaberLS <patryk.platek19@gmail.com>
2025-12-16 21:17:06 +0100

    feat(user): add repository annotation

--- src/main/java/pl/wszib/java/advanced/database/UserRepository.java
+++ src/main/java/pl/wszib/java/advanced/database/UserRepository.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import lombok.Getter;
 import pl.wszib.java.advanced.model.User;
 
+@Repository
 @Getter
 public class UserRepository implements IUserRepository {
   private final List<User> users = new ArrayList<>();
@@ -19,5 +22,4 @@ public class UserRepository implements IUserRepository {
 
     return Optional.empty();
   }
-
 }
