a28ae5940a26c407f7773263d0dd95c79429e309
SaberLS <patryk.platek19@gmail.com>
2025-12-16 21:08:53 +0100

    feat: run core with context

--- src/main/java/pl/wszib/java/advanced/App.java
+++ src/main/java/pl/wszib/java/advanced/App.java
@@ -1,7 +1,17 @@
 package pl.wszib.java.advanced;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import pl.wszib.java.advanced.configuration.AppConfiguration;
+import pl.wszib.java.advanced.core.Core;
+import pl.wszib.java.advanced.core.ICore;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("SIEMA");
+    try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+        AppConfiguration.class)) {
+      ICore core = context.getBean(Core.class);
+      core.run();
+    }
   }
 }
