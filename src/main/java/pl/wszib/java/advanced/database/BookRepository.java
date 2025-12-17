e7284da8aebe64a9f76d137b590cf09cdd5b163a
SaberLS <patryk.platek19@gmail.com>
2025-12-17 10:53:38 +0100

    feat(book): add remove/add book methods

--- src/main/java/pl/wszib/java/advanced/database/BookRepository.java
+++ src/main/java/pl/wszib/java/advanced/database/BookRepository.java
@@ -2,11 +2,11 @@ package pl.wszib.java.advanced.database;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import lombok.Getter;
-import pl.wszib.java.advanced.exceptions.CanNotBorrowBookException;
 import pl.wszib.java.advanced.model.Book;
 
 @Repository
@@ -19,7 +19,7 @@ public class BookRepository implements IBookRepository {
     for (Book book : this.getBooks())
       if (book.getIsbn().equals(isbn)) {
         if (!book.isAvailable()) {
-        book.setAvailable(false);
+          book.setAvailable(false);
           return true;
         }
         return false;
@@ -27,5 +27,18 @@ public class BookRepository implements IBookRepository {
 
     return false;
   }
+
+  @Override
+  public boolean addBook(Book book) {
+    return this.books.add(book);
+  }
+
+  @Override
+  public Optional<Book> removeBook(String isbn) {
+    for (int i = 0; i < this.books.size(); i++)
+      if (this.books.get(i).getIsbn().equals(isbn))
+        return Optional.of(this.books.remove(i));
+
+    return Optional.empty();
   }
 }
--- src/main/java/pl/wszib/java/advanced/database/IBookRepository.java
+++ src/main/java/pl/wszib/java/advanced/database/IBookRepository.java
@@ -1,11 +1,16 @@
 package pl.wszib.java.advanced.database;
 
 import java.util.List;
+import java.util.Optional;
 
 import pl.wszib.java.advanced.model.Book;
 
 public interface IBookRepository {
   boolean borrowBook(String isbn);
+
+  boolean addBook(Book book);
+
+  Optional<Book> removeBook(String isbn);
 
   public List<Book> getBooks();
 }
